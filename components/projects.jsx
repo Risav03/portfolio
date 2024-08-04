6c564a032f666979ff9d0fa15e3a3d23b2eaa851
Risavdeb Patra <95542615+Risav03@users.noreply.github.com>
2024-08-04 15:40:57 +0530

    Update projects.jsx

--- components/projects.jsx
+++ components/projects.jsx
@@ -5,6 +5,7 @@ export const Projects = () => {
   return (
     <div className='flex flex-col items-center justify-center w-screen my-10 md:-translate-x-16 -translate-x-4'>
         <Heading>Projects</Heading>
+    <div>----Being updated----</div>
     </div>
   )
 }
